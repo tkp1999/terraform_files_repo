05ad3bc3ad4f67de1f471673654b6789a85b2264
Tarun Kumar Pal <tarunkumarpal468@gmail.com>
2023-08-11 14:28:55 +0530

    created vpc ans subnet using terraform

--- aws/vpc_project/inputs.tf
+++ aws/vpc_project/inputs.tf
@@ -3,3 +3,8 @@ variable "vpc_network_cidr" {
   default     = "10.10.0.0/16"
   description = "This is network cidr"
 }
+variable "web1-subnet-cidr" {
+  type        = string
+  default     = "10.10.0.0/24"
+  description = "this is web1 subnet cidr"
+}
--- aws/vpc_project/main.tf
+++ aws/vpc_project/main.tf
@@ -4,3 +4,13 @@ resource "aws_vpc" "ntier-vpc" {
     Name = local.name
   }
 }
+resource "aws_subnet" "web1-subnet" {
+  vpc_id     = aws_vpc.ntier-vpc.id
+  cidr_block = var.web1-subnet-cidr
+  tags = {
+    Name = "web1"
+  }
+  depends_on = [
+    aws_vpc.ntier-vpc
+  ]
+}
